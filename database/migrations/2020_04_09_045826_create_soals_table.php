895dc0eee2d327d7128c19d7b19a8d7d61ca7551
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-04-09 12:10:53 +0700

    INIT

---
+++ database/migrations/2020_04_09_045826_create_soals_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateSoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soals', function (Blueprint $table) {
+            $table->id();
+            $table->string('pertanyaan');
+            $table->string('option_1');
+            $table->string('option_2');
+            $table->string('option_3');
+            $table->string('option_4');
+            $table->string('option_5');
+            $table->string('jawaban');
+
+            $table->integer('id_prodi');
+            // POSTGREE TIMESTAMP
+            // $table->timestamps();
+
+            // MY SQL TIMESTAMP
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soals');
+    }
+}
