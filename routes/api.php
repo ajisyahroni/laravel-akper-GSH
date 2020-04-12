d3cd00f78fadf9f441d68aa452b415e75367534c
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-04-12 15:56:29 +0700

    UPDATE: add forces scheme for https conn.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// FORCES HTTPS SCHEMA
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
