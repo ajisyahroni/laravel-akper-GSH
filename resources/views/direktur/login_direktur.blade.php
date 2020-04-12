5002f03a9e9d3236fa79534baca620a0f9c7cb5d
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-04-12 16:04:32 +0700

    delete header on direktur view

--- resources/views/direktur/login_direktur.blade.php
+++ resources/views/direktur/login_direktur.blade.php
@@ -5,78 +5,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   <!-- DataTables -->
-   <link rel="stylesheet" href="/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-   <link rel="stylesheet" href="/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="/dist/css/adminlte.min.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <style>
-      .lunox{
-          margin-top: 60px;
-      }
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="/dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <style>
+        .lunox {
+            margin-top: 60px;
+        }
 
-      .zilong{
-          width: 290px;
-      }
+        .zilong {
+            width: 290px;
+        }
 
-      .balmond{
-          font-family: Arial, Helvetica, sans-serif;
-      }
-  </style>
+        .balmond {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
     <title>Login Direktur</title>
 </head>
 
 <body>
     <div class="container">
-        <div class="mt-3 balmond ">
-            <h2>AKPER GSH</h2>
-        </div>
         <hr>
         <div class="row">
             <div class="col-sm">
             </div>
             <div class="col-sm lunox">
                 <div class="card">
-                    <div class="ml-5 mr-5 mb-5 mt-5" >
+                    <div class="ml-5 mr-5 mb-5 mt-5">
                         <div class="row">
                             <div class="col">
                             </div>
                             <div class="col">
-                                <img  style="width: 120px;" src="../../img/login.png" alt="">
+                                <img style="width: 120px;" src="../../img/login.png" alt="">
                             </div>
                             <div class="col">
                             </div>
                         </div>
-                            
-                        <form>    
+
+                        <form action="/direktur/view/dashboard">
                             <div class="form-group  ">
-                              <label for="exampleInputEmail1">Email</label>
-                              <input type="email" class="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="masukan email" >
+                                <label for="exampleInputEmail1">Email</label>
+                                <input type="email" class="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="masukan email">
                             </div>
                             <div class="form-group">
-                              <label for="exampleInputPassword1">Password</label>
-                              <input type="password" class="form-control" required id="exampleInputPassword1" placeholder="masukan password" >
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" required id="exampleInputPassword1" placeholder="masukan password">
                             </div>
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary zilong">Login</button> 
-                              </div>
-                          </form>         
+                                <button type="submit" class="btn btn-danger zilong">Login</button>
+                            </div>
+                        </form>
                     </div>
-                </div>              
+                </div>
             </div>
             <div class="col-sm">
             </div>
         </div>
-       
+
     </div>
-    
+
 </body>
 <script src="../../plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
@@ -90,4 +87,5 @@
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
+
 </html>
