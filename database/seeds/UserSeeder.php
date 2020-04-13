812ced95559786242ca4a7b2313dc5e045901119
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-04-13 11:41:56 +0700

    UPDATE: change seed with passwd

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,33 +14,33 @@ class UserSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create('id_ID');
-        for($i =0;$i<100;$i++){
+        for ($i = 0; $i < 100; $i++) {
 
-        
-        
-        DB::table('users')->insert([
-            'nama' => $faker->name,
-            'email' => $faker->email,
-            'nik' => '454554545456',
-            'tempat_lahir' => $faker->city,
-            'tanggal_lahir' =>  $faker->date,
-            'agama' => 'islam',
-            'kewarganegaraan' => 'wni',
-            'telepon' =>  '082138308595',
-            'nama_ibu_kandung' => $faker->name,
-            'nama_ayah_kandung' => $faker->name,
-            'tinggi_badan' => 147,
-            'berat_badan' => 74,
-            'foto_url' => './foto',
-            'ijazah_url' => './ijazah',
-            'kota' => $faker->city,
-            'kecamatan' => $faker->city,
-            'alamat' => $faker->city,
-            'kodepos' => 57694,
-            'pendidikan_terakhir' => 'sma/ipa',
-            'rata_nem' => 78.5,
 
-        ]);
-    }
+
+            DB::table('users')->insert([
+                'nama' => $faker->name,
+                'email' => $faker->email,
+                'nik' => '454554545456',
+                'tempat_lahir' => $faker->city,
+                'tanggal_lahir' =>  $faker->date,
+                'agama' => 'islam',
+                'kewarganegaraan' => 'wni',
+                'telepon' =>  '082138308595',
+                'nama_ibu_kandung' => $faker->name,
+                'nama_ayah_kandung' => $faker->name,
+                'tinggi_badan' => 147,
+                'berat_badan' => 74,
+                'foto_url' => './foto',
+                'ijazah_url' => './ijazah',
+                'kota' => $faker->city,
+                'kecamatan' => $faker->city,
+                'alamat' => $faker->city,
+                'kodepos' => 57694,
+                'pendidikan_terakhir' => 'sma/ipa',
+                'rata_nem' => 78.5,
+                'password' => bcrypt('secret')
+            ]);
+        }
     }
 }
