49fc8fb6b827bf9bbc66ef92762c45ddb57f3065
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-04-21 13:14:08 +0700

    UPDATE: remove unused section

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -168,47 +168,7 @@
       </div>
     </section><!-- End Services Section -->
 
-    <!-- ======= Clients Section ======= -->
-    <div class="container">
-      <div class="section-title" data-aos="fade-up">
-        <h2>Biaya Kuliah</h2>
-      </div>
-      <div class="row my-4">
-
-
-        <table class="table my-3">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Jurusan</th>
-              <th scope="col">SPP Tetap</th>
-              <th scope="col">SPP Variabel</th>
-              <th scope="col">Pembangunan</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    </div><!-- End Clients Section -->
+    
 
   </main><!-- End #main -->
 
