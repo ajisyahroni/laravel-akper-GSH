2713472f5d6491fb51f8c60524628ffcc0cffffa
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-07-31 22:40:05 +0700

    UPDATE: set host when fetch koreksi

--- resources/views/user/try_test_user.blade.php
+++ resources/views/user/try_test_user.blade.php
@@ -215,7 +215,7 @@
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     }
                 }
-                fetch('{{ url("api/user/action/koreksi/test") }}', opt)
+                    fetch('{{ request()->getSchemeAndHttpHost() }}' + '/user/action/koreksi/test',opt)
                     .then(r => {
                         if (r.ok) {
                             localStorage.clear();
@@ -251,7 +251,7 @@
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     }
                 }
-                fetch('{{ url("api/user/action/koreksi/test") }}', opt)
+                    fetch('{{ request()->getSchemeAndHttpHost() }}' + '/user/action/koreksi/test',opt)
                     .then(r => {
                         if (r.ok) {
                             localStorage.clear();
